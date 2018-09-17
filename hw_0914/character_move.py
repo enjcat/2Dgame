9cf36fece67fee5ab4ca9402e7fd972be0392e55
enjcat <tlawltjq0110@naver.com>
2018-09-18 00:48:52 +0900

    0914 수정
    계속 반복되게 수정함

--- hw_0914/character_move.py
+++ hw_0914/character_move.py
@@ -8,55 +8,57 @@ character_right = load_image('run_right.png')
 character_up = load_image('run_up.png')
 character_left = load_image('run_left.png')
 
-frame = 0
-x = 0
-while (x < 800):
-    clear_canvas()
-    grass.draw(400, 30)
-    character_down.clip_draw(frame * 100, 0, 100, 100, x, 90)
-    frame = (frame + 1) % 8
-    update_canvas()
-    x = x + 10
-    delay(0.05)
-    get_events()
+while(1):
 
-frame = 0
-x = 0
+    frame = 0
+    x = 0
+    while (x < 800):
+        clear_canvas()
+        grass.draw(400, 30)
+        character_down.clip_draw(frame * 100, 0, 100, 100, x, 90)
+        frame = (frame + 1) % 8
+        update_canvas()
+        x = x + 10
+        delay(0.05)
+        get_events()
 
-while (x < 600):
-    clear_canvas()
-    grass.draw(400, 30)
-    character_right.clip_draw(0, frame * 100, 100, 100, 770, x)
-    frame = (frame + 1) % 8
-    update_canvas()
-    x = x + 10
-    delay(0.05)
-    get_events()
+    frame = 0
+    x = 0
 
-frame = 0
-x = 0
+    while (x < 600):
+        clear_canvas()
+        grass.draw(400, 30)
+        character_right.clip_draw(0, frame * 100, 100, 100, 770, x)
+        frame = (frame + 1) % 8
+        update_canvas()
+        x = x + 10
+        delay(0.05)
+        get_events()
 
-while (x < 800):
-    clear_canvas()
-    grass.draw(400, 30)
-    character_up.clip_draw(frame * 100, 0, 100, 100, 800-x, 570)
-    frame = (frame + 1) % 8
-    update_canvas()
-    x = x + 10
-    delay(0.05)
-    get_events()
+    frame = 0
+    x = 0
 
-frame = 0
-x = 0
+    while (x < 800):
+        clear_canvas()
+        grass.draw(400, 30)
+        character_up.clip_draw(frame * 100, 0, 100, 100, 800-x, 570)
+        frame = (frame + 1) % 8
+        update_canvas()
+        x = x + 10
+        delay(0.05)
+        get_events()
 
-while (x < 600):
-    clear_canvas()
-    grass.draw(400, 30)
-    character_left.clip_draw(0, frame * 100, 100, 100, 30, 600-x)
-    frame = (frame + 1) % 8
-    update_canvas()
-    x = x + 10
-    delay(0.05)
-    get_events()
+    frame = 0
+    x = 0
+
+    while (x < 600):
+        clear_canvas()
+        grass.draw(400, 30)
+        character_left.clip_draw(0, frame * 100, 100, 100, 30, 600-x)
+        frame = (frame + 1) % 8
+        update_canvas()
+        x = x + 10
+        delay(0.05)
+        get_events()
 
 close_canvas()
