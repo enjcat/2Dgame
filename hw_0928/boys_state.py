87f8e6e941e722316dc2f8f09a9f6b970f6955eb
enjcat <tlawltjq0110@naver.com>
2018-10-02 12:10:43 +0900

    hw_0928
    게임 프레임워크 과제

---
+++ hw_0928/boys_state.py
@@ -0,0 +1,136 @@
+from pico2d import *
+import game_framework
+import random
+
+
+class Dot:
+    def __init__(self,xDot,yDot):
+        self.xDot = xDot
+        self.yDot = yDot
+
+
+class Grass:
+    def __init__(grass_):
+        grass_.image = load_image('./grass.png')
+        print(grass_.image)
+    def draw(grass_):
+        grass_.image.draw(400, 300)
+
+class Boy:
+    def __init__(self):
+        print("Creating..")
+        self.count = 0
+        self.Bool = False
+        self.x = random.randint(0, 800)
+        self.y = random.randint(0, 600)
+        self.speed = random.uniform(1.0, 5.0)
+        self.frame = random.randint(0, 7)
+        self.image = load_image('./run_animation.png')
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+    def update(self):
+        global DirList
+        global gloX, gloY
+        gloX_, gloY_ = gloX,gloY
+        self.frame = (self.frame + 1) % 8
+        if DirList:
+            if len(DirList) > self.count:
+                gloX_ = DirList[self.count].xDot
+                gloY_ = DirList[self.count].yDot
+                self.Bool = True
+            else:
+                self.Bool = False
+        else:
+                self.Bool = False
+
+        lengthX = gloX_ - self.x
+        lengthY = gloY_ - self.y
+
+        dist = math.sqrt(lengthX ** 2 + lengthY ** 2) 
+
+        if lengthX < 0:
+            PathX = -1
+        else:
+            PathX = 1
+            
+        if lengthY < 0:
+            PathY = -1
+        else:
+            PathY = 1
+
+        if dist > 0:
+            self.x += self.speed * lengthX/ dist 
+            self.y += self.speed * lengthY/ dist
+            
+
+        if lengthX*PathX < self.speed:
+            self.x = gloX_
+        if lengthY*PathY < self.speed:
+            self.y = gloY_
+
+        if self.Bool == True:
+            if self.x == DirList[self.count].xDot and self.y == DirList[self.count].yDot:
+                self.count += 1
+
+
+def handle_events():
+    global running
+    global gloX,gloY
+    global index
+    global DirList
+    global boys
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                game_framework.pop_state()
+        elif event.type == SDL_MOUSEMOTION:
+             gloX,gloY = event.x, 600 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            DirList.append(Dot(event.x, 600-event.y))
+            print(DirList)
+            index += 1
+       
+
+DirList=[]
+index=0
+gloX=0
+gloY=0
+Grass_=0
+boys = []
+def enter():    
+    open_canvas()
+    global DirList, index, running, gloX,gloY,Grass_, boys
+    DirList = []
+    index = 0
+    gloX,gloY = 0,0
+    Grass_ = Grass()
+    boys = [ Boy() for i in range(20) ]
+
+def update():
+    global boys
+    for boy in boys:
+        boy.update()
+
+def draw():
+    delay(0.03)
+    global Grass_,boys
+    clear_canvas()
+    Grass_.draw()
+    for boy in boys:
+        boy.draw()
+    update_canvas()
+
+
+def exit():
+    close_canvas()
+
+def pause():
+    pass
+
+def resume():
+    pass
+
+
